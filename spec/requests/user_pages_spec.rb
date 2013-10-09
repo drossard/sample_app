58337eb5a915373a9ad85e2bb3abbb301d09e941
Kevin L. Drossard <kevin@erdee.de>
2013-10-09 16:24:14 +0200

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -50,7 +50,8 @@ describe "signup" do
       describe "after saving user" do
         before { click_button submit }
         let (:user) { User.find_by(email: "user@example.com") }
-
+        
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector("div.alert.alert-success", text: "Welcome") }
       end
